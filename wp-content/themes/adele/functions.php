9fe04137f105670192b9fabef9204fe01355c611
AdeleH3 <152315868+AdeleH3@users.noreply.github.com>
2026-03-13 20:46:25 +0000

    add products

--- wp-content/themes/adele/functions.php
+++ wp-content/themes/adele/functions.php
@@ -11,8 +11,8 @@ function theme_enqueue_styles() {
         array('parent-style')
     );
     wp_enqueue_script('custom_javascript0', get_stylesheet_directory_uri() . '/customElements.js');
-    wp_enqueue_script_module('custom_javascript1', "https://rhildred.github.io/UX308Assignment1/");
-    wp_enqueue_script_module('custom_javascript2', "https://rhildred.github.io/UX308Assignment1/");
+    wp_enqueue_script_module('custom_javascript1', "https://adeleh3.github.io/UX308-Assignment1/fab.js");
+    wp_enqueue_script_module('custom_javascript2', "https://adeleh3.github.io/UX308-Assignment1/chat.js");
 }
 //
 // Your code goes below
